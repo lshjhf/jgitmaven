cbec85319934e7f4019bd988595d0fc7c91067a5
lshjhf <76999979+lshjhf@users.noreply.github.com>
2021-07-02 13:13:20 +0800

    Update Main.java

--- src/main/java/jgitdeamo/Main.java
+++ src/main/java/jgitdeamo/Main.java
@@ -100,6 +100,6 @@ public class Main {
     public static String getOS() {
         String os = System.getProperty("os.name");
         System.out.println(os);
-        return os;
+        return /*998*/os;
     }
 }
